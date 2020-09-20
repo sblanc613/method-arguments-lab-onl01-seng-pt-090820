d5ad3c76e666717f4ea3b9b5f6edc0f47b0b3dcb
Samuel Blanc <sblanc613@gmail.com>
2020-09-20 23:12:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction(name,language)
-  puts "Hi, my name is #{introduction_with_language("Dan","Ember.js")}."
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name}, #{Ember.js}."
 end
